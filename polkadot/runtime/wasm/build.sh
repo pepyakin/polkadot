a222aa6df0f4e0d976f26b8e76737fb31cec89cf
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-03-22 18:42:50 +0300

    export tables from other .wasm files

--- polkadot/runtime/wasm/build.sh
+++ polkadot/runtime/wasm/build.sh
@@ -4,5 +4,8 @@ set -e
 cargo +nightly build --target=wasm32-unknown-unknown --release
 for i in polkadot_runtime
 do
-	wasm-gc target/wasm32-unknown-unknown/release/$i.wasm target/wasm32-unknown-unknown/release/$i.compact.wasm
+	# Add an export entry for the table defined in the module.
+	wasm-export-table target/wasm32-unknown-unknown/release/$i.wasm target/wasm32-unknown-unknown/release/$i.export_table.wasm
+	cp target/wasm32-unknown-unknown/release/$i.export_table.wasm target/wasm32-unknown-unknown/release/$i.wasm
+	wasm-gc target/wasm32-unknown-unknown/release/$i.export_table.wasm target/wasm32-unknown-unknown/release/$i.compact.wasm
 done
--- substrate/executor/wasm/build.sh
+++ substrate/executor/wasm/build.sh
@@ -4,5 +4,8 @@ set -e
 cargo +nightly build --target=wasm32-unknown-unknown --release
 for i in test
 do
-	wasm-gc target/wasm32-unknown-unknown/release/runtime_$i.wasm target/wasm32-unknown-unknown/release/runtime_$i.compact.wasm
+	# Add an export entry for the table defined in the module.
+	wasm-export-table target/wasm32-unknown-unknown/release/runtime_$i.wasm target/wasm32-unknown-unknown/release/runtime_$i.export_table.wasm
+	cp target/wasm32-unknown-unknown/release/runtime_$i.export_table.wasm target/wasm32-unknown-unknown/release/runtime_$i.wasm
+	wasm-gc target/wasm32-unknown-unknown/release/runtime_$i.export_table.wasm target/wasm32-unknown-unknown/release/runtime_$i.compact.wasm
 done
